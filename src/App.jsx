f7d335f5bdb863bbc2c68e76ef83d294b6189d0c
MarcoProstamo <Marcopros1201@gmail.com>
2024-12-03 15:53:52 +0100

    Feat → Post Creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
-import { useState } from "react";
 import Header from "./assets/components/Header";
 import Main from "./assets/components/Main";
 
 function App() {
-  const [inputValue, setInputValue] = useState("");
-
-  function handleChange(e) {
-    setInputValue(e.target.value);
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-  }
-
   return (
     <>
       <Header />
       <hr />
-      <Main
-        handleSubmit={handleSubmit}
-        handleChange={handleChange}
-        inputValue={inputValue}
-      />
+      <Main />
     </>
   );
 }
--- src/assets/components/Main.jsx
+++ src/assets/components/Main.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
 import PostList from "./PostList";
-import { posts } from "../data/posts";
+import { posts as data } from "../data/posts";
 
-export default function Main({ handleSubmit, handleChange, inputValue }) {
+export default function Main() {
+  const [titleInput, setTitleInput] = useState("");
+  const [authorInput, setAuthorInput] = useState("");
+  const [statusInput, setStatusInput] = useState("");
+  const [contentInput, setContentInput] = useState("");
+
+  const [filterInput, setFilterInput] = useState("");
+
+  const [posts, setPosts] = useState(data);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newPost = {
+      title: titleInput,
+      content: contentInput,
+      status: statusInput,
+      author: authorInput,
+      id: posts.length + 1,
+    };
+    const newPosts = [...posts, newPost];
+    setPosts(newPosts);
+    console.log(newPosts);
+  }
+
   return (
     <main>
       <div className="container">
@@ -11,32 +36,38 @@ export default function Main({ handleSubmit, handleChange, inputValue }) {
             Post Creation
           </div>
           <div className="col-12">
-            <form className="row gap-3">
+            <form onSubmit={handleSubmit} className="row gap-3">
               <div className="col-12">
                 <div className="row gap-3">
-                  <div className="col p-0 form-floating">
+                  <div className="col form-floating">
                     <input
                       type="text"
                       className="form-control"
+                      value={titleInput}
+                      onChange={(e) => setTitleInput(e.target.value)}
                       placeholder="Title..."
                     />
-                    <label>Title...</label>
+                    <label className="py-3 px-4">Title...</label>
                   </div>
-                  <div className="col p-0 form-floating">
+                  <div className="col form-floating">
                     <input
                       type="text"
                       className="form-control"
+                      value={authorInput}
+                      onChange={(e) => setAuthorInput(e.target.value)}
                       placeholder="Author..."
                     />
-                    <label>Author...</label>
+                    <label className="py-3 px-4">Author...</label>
                   </div>
-                  <div className="col p-0 form-floating">
+                  <div className="col form-floating">
                     <input
                       type="text"
                       className="form-control"
+                      value={statusInput}
+                      onChange={(e) => setStatusInput(e.target.value)}
                       placeholder="Status..."
                     />
-                    <label>Status...</label>
+                    <label className="py-3 px-4">Status...</label>
                   </div>
                 </div>
               </div>
@@ -45,10 +76,15 @@ export default function Main({ handleSubmit, handleChange, inputValue }) {
                   <textarea
                     type="text"
                     className="form-control"
+                    value={contentInput}
+                    onChange={(e) => setContentInput(e.target.value)}
                     placeholder="Content..."
                   />
                   <label>Content...</label>
                 </div>
+              </div>
+              <div className="col-12">
+                <button className="btn btn-primary">Create</button>
               </div>
             </form>
           </div>
@@ -66,8 +102,8 @@ export default function Main({ handleSubmit, handleChange, inputValue }) {
                 type="search"
                 className="form-control"
                 placeholder="Search..."
-                value={inputValue}
-                onChange={handleChange}
+                value={filterInput}
+                onChange={(e) => setFilterInput(e.target.value)}
               />
               <label>Search...</label>
             </div>
@@ -76,7 +112,7 @@ export default function Main({ handleSubmit, handleChange, inputValue }) {
 
         {/* Posts Output */}
         <section className="d-flex flex-column gap-3">
-          <PostList postList={posts} filteringTerm={inputValue} />
+          <PostList postList={posts} filteringTerm={filterInput} />
         </section>
       </div>
     </main>
