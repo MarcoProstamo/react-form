8a7036270008c66f4e2f6bdcb57a3a6648d5e5f4
MarcoProstamo <Marcopros1201@gmail.com>
2024-12-03 16:32:22 +0100

    Feat → Edit Title

--- src/assets/components/Post.jsx
+++ src/assets/components/Post.jsx
@@ -1,15 +1,54 @@
+import { useState } from "react";
+
 export default function Post({ post, handleClick }) {
+  const [newTitleInput, setNewTitleInput] = useState("");
+  function handleEditClick(e) {
+    e.preventDefault();
+
+    post.title = newTitleInput;
+    setNewTitleInput("");
+  }
+
   return (
     <div className="card shadow">
       <div className="card-body">
         <h2 className="fw-semibold">{post.title}</h2>
         <p className="fs-5">{post.content}</p>
-        <button
-          className="btn btn-danger shadow"
-          onClick={() => handleClick(post.id)}
-        >
-          Delete
-        </button>
+        <div className="row">
+          <div className="col-9">
+            <form className="row">
+              <div className="col-8">
+                <div className="div p">
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={newTitleInput}
+                    placeholder="New Title..."
+                    onChange={(e) => setNewTitleInput(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="col-4">
+                <button
+                  className="btn btn-secondary ms-1"
+                  onClick={handleEditClick}
+                >
+                  Edit Title
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="col-3">
+            <div className="d-flex justify-content-end">
+              <button
+                className="btn btn-danger shadow"
+                onClick={() => handleClick(post.id)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
