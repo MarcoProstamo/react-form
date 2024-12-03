7f26606a7f3a0437553b96d537cb9fc1dcccfa9f
MarcoProstamo <Marcopros1201@gmail.com>
2024-12-03 15:26:56 +0100

    Add → Post Creation Section

--- src/assets/components/Main.jsx
+++ src/assets/components/Main.jsx
@@ -4,28 +4,81 @@ import { posts } from "../data/posts";
 export default function Main({ handleSubmit, handleChange, inputValue }) {
   return (
     <main>
-      <section>
-        <form
-          onSubmit={handleSubmit}
-          className="d-flex justify-content-center my-4"
-        >
-          <div></div>
-          <div className="form-floating">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Search..."
-              value={inputValue}
-              onChange={handleChange}
-            />
-            <label htmlFor="inputText">Search...</label>
+      <div className="container">
+        {/* Add Post */}
+        <section className="row gap-3">
+          <div className="col-12 text-center fw-semibold fs-4">
+            Post Creation
           </div>
-        </form>
-      </section>
+          <div className="col-12">
+            <form className="row gap-3">
+              <div className="col-12">
+                <div className="row gap-3">
+                  <div className="col p-0 form-floating">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Title..."
+                    />
+                    <label>Title...</label>
+                  </div>
+                  <div className="col p-0 form-floating">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Author..."
+                    />
+                    <label>Author...</label>
+                  </div>
+                  <div className="col p-0 form-floating">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Status..."
+                    />
+                    <label>Status...</label>
+                  </div>
+                </div>
+              </div>
+              <div className="col-12">
+                <div className="form-floating">
+                  <textarea
+                    type="text"
+                    className="form-control"
+                    placeholder="Content..."
+                  />
+                  <label>Content...</label>
+                </div>
+              </div>
+            </form>
+          </div>
+        </section>
+      </div>
+
       <hr />
-      <section className="container d-flex flex-column gap-3 mt-5">
-        <PostList postList={posts} filteringTerm={inputValue} />
-      </section>
+
+      <div className="container">
+        {/* Filter Post */}
+        <section className="d-flex justify-content-center my-4">
+          <form role="search">
+            <div className="form-floating">
+              <input
+                type="search"
+                className="form-control"
+                placeholder="Search..."
+                value={inputValue}
+                onChange={handleChange}
+              />
+              <label>Search...</label>
+            </div>
+          </form>
+        </section>
+
+        {/* Posts Output */}
+        <section className="d-flex flex-column gap-3">
+          <PostList postList={posts} filteringTerm={inputValue} />
+        </section>
+      </div>
     </main>
   );
 }
